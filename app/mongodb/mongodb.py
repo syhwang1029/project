1bb7788ee97243f6f4e4e38dad0a21bc44434698
syhwang1029 <syhwang@itall.com>
2024-12-06 08:55:32 +0900

    오타수정

--- app/mongodb/mongodb.py
+++ app/mongodb/mongodb.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient # MongoDB 연동
 # pymongo 동기적 연결 
 # https://wooiljeong.github.io/python/mongodb-01/ 
 
-# MongoDB connetcion 연결
+# MongoDB Connection 연결
 # MongoDB Client 전용 URI
 mongodb_uri = "mongodb://localhost:27017/"
 client = MongoClient(mongodb_uri)
