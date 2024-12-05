c3c01b31df69a365642ca69755e3302a345aa31e
syhwang1029 <syhwang@itall.com>
2024-12-05 09:05:39 +0900

    오타 수정

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI  #FastAPI 서버
-from app.controller.user_controller import UserController #mongobd와 FastAPI 연동용 컨트롤러
+from app.controller.user_controller import UserController #mongodb와 FastAPI 연동용 컨트롤러
 
 import uvicorn #main 함수로 uvicorn 자동 실행 
 
@@ -18,11 +18,11 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/") #mongobd 테스트용
+@app.get("/") #mongodb 테스트용
 def test():
     return{"test keys":"tes values"}
 
-@app.post("/test") #mongobd 연결용 컨트롤러
+@app.post("/test") #mongodb 연결용 컨트롤러
 async def test():
     test = await user_controller.test()
     return test
