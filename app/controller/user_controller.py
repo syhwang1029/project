2b1d60369a62f8fc71f8b2342a358300c8608682
syhwang1029 <syhwang@itall.com>
2024-12-05 10:23:25 +0900

    fastapi와 mongodb 연동 test 컨트롤러 -> 라우터로 변경 완료, test 성공

--- app/controller/user_controller.py
+++
@@ -1,5 +0,0 @@
-class UserController : #mongobd와 fastapi 연동용 컨트롤러
-    async def test(self):
-        return "test 성공!!"
-    
-    # controller class 사용x -> route 추가
---
+++ app/router/mongodb_router.py
@@ -0,0 +1,4 @@
+class MongoDBRouter : #mongobd와 fastapi 연동용 라우터
+    async def test(self):
+        return "test 성공!!"
+    
