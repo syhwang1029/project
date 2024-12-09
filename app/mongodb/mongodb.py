a780f38a14191a2f77ae5e05432d141e7b706f07
syhwang1029 <syhwang@itall.com>
2024-12-09 17:37:50 +0900

    mongodb

--- app/mongodb/mongodb.py
+++ app/mongodb/mongodb.py
@@ -4,7 +4,36 @@ from pymongo import MongoClient # MongoDB 연동
 
 # MongoDB Connection 연결
 # MongoDB Client 전용 URI
-mongodb_uri = "mongodb://localhost:27017/"
-client = MongoClient(mongodb_uri)
 
-db = client #mongodb uri를 db 객체에 저장
+# MongoDB 클라이언트 생성
+def create_mongo_client(host='localhost', port=27017):
+    client = MongoClient(host, port)
+    return client
+# mongodb_uri = "mongodb://localhost:27017/"
+#client = MongoClient(mongodb_uri)
+
+#database와 collection 선택
+def get_database(client, datanase):
+    db = client[datanase]
+    return db
+
+# db = client["database"] #db 선택
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+# user_collection = db["user"] #collection 선택
+
+#mongodb 연결
+# 1. database 가져오기
+# db = client["db_name"] 
+# 2. collection 가져오기
+# collection = db["collection_name"]
+# mongodb 연결 참조
+# https://it-creamstory.tistory.com/entry/Python-MongoDB-connect-%EC%97%B0%EA%B2%B0%ED%95%98%EA%B8%B0
+
+# 문서 삽입(insert)
+def insert_user(collection, user):
+    collection.insert_one(user)
+
+# 문서 검색(find)    
+def find_user(collection, query):
+    user = collection.find_one(query)
+    return user
