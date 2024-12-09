89a344d33e5acc25c1eee0388a5d0ab1bfe1917e
syhwang1029 <syhwang@itall.com>
2024-12-09 17:37:29 +0900

    user model

--- app/model/user.py
+++ app/model/user.py
@@ -1,10 +1,9 @@
-from app.mongodb.mongodb import db #mongodb 호출
 from pydantic import BaseModel # BaseModel: 데이터 모델 정의
 # None : 기본, 선택적 type 설정
+from bson import objectid
 
 class User(BaseModel): #user 데이터 모델 정의
-    name: str 
-    id: str 
+    _id: objectid
+    uname: str 
+    email: str 
     password: str 
-
-user = db['user_info']
