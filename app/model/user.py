f9811f25a75974f3a12c2dd190aa75f666d7507c
syhwang1029 <syhwang@itall.com>
2024-12-10 17:46:25 +0900

    user basemodel

--- app/model/user.py
+++ app/model/user.py
@@ -1,9 +1,8 @@
+# user schema
 from pydantic import BaseModel # BaseModel: 데이터 모델 정의
 # None : 기본, 선택적 type 설정
-from bson import objectid
 
 class User(BaseModel): #user 데이터 모델 정의
-    _id: objectid
-    uname: str 
+    name: str 
     email: str 
     password: str 
