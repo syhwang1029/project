7ff292686444da6b7962a9c7c1e45eb4919a4a16
syhwang1029 <syhwang@itall.com>
2024-12-11 11:56:51 +0900

    user model

--- app/model/user.py
+++ app/model/user.py
@@ -1,8 +1,17 @@
 # user schema
-from pydantic import BaseModel # BaseModel: 데이터 모델 정의
-# None : 기본, 선택적 type 설정
+from bson import ObjectId
+from pydantic import BaseModel, Field # Database 모델의 정의
+# 타입 검증, 오류 관리
+# https://mobicon.tistory.com/627
 
-class User(BaseModel): #user 데이터 모델 정의
+# bson -> json 인코딩 역할 
+
+class User(BaseModel): # user 데이터 모델 정의
     name: str 
+    email: str
+    password: str
+    
+class CreateUser(User): # 생성
+    name : str
     email: str 
     password: str 
