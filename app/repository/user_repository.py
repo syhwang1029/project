0f522fc53349e9a0e66fe11811db467f5c82a47d
syhwang1029 <syhwang@itall.com>
2024-12-11 11:57:57 +0900

    uset repository dict, list 수정

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -2,22 +2,18 @@
 # db 관련
 
 from app.mongodb.mongodb import client #mongodb cilent 연결
-from app.model.user import User
-db = client["database"] #database 선택 
-db["user"] #collection 선택
-# router
 
-def user_entity(db) -> dict:
+
+db = client["database"] # database 선택 
+collection = db["user"] # collection 선택
+
+def user_entity(user) -> dict:
     return{
-        "_id":str(db['_id'].value()),
-        "name": db['name'],
-        "email": db['email'],
-        "password": db['password']    
+        "id": str(user["_id"]),
+        "name": str(user["name"]),
+        "email": str(user["email"]),
+        "password": str(user["password"]),   
     }
-    
-def user_list(db) -> list:
-    user_list = []
-    for User in db: 
-        user_list.append(user_entity(User))
-    return user_list
-    
+
+def user_list(users) -> list:
+    return [user_entity(user) for user in users]
