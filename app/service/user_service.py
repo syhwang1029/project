db5708178f1778d69636a391ae906a6e2533b396
syhwang1029 <syhwang@itall.com>
2024-12-11 11:57:27 +0900

    user service jwt 추가

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,6 +1,11 @@
-from model.user import User
-class User:
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
+from passlib.context import CryptContext # bcrypt
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# 일반 텍스트 비밀번호 hashing
+def hash_password(password : str):
+    return pwd_context.hash(password)
+
+# 비밀번호 검증
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
