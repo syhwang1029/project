7ef609ee419894b49ad2fe7f2b49c71a84a7293c
syhwang1029 <syhwang@itall.com>
2024-12-11 17:38:52 +0900

    user service

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,11 +1,22 @@
-from passlib.context import CryptContext # bcrypt
+from app.repository.user_repository import *
+from app.model.user import *
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# service 의존성 주입 참고 
+# https://the-boxer.tistory.com/63
 
-# 일반 텍스트 비밀번호 hashing
-def hash_password(password : str):
-    return pwd_context.hash(password)
-
-# 비밀번호 검증
-def verify_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+# user service
+class UserService:
+    def __init__(self): # service가 repository에 의존
+        self.user_repository = UserRepository()
+        
+    def create_user(self): # 생성
+        return self.user_repository.create_user(User)
+    
+    def read_user(self): # 조회
+        return self.user_repository.read_user(User)
+    
+    def update_user(self): # 수정
+        return self.user_repository.update_user(UserUpdate)
+    
+    def delete_user(self): # 삭제
+        return self.user_repository.delete_user(UserDelete)
