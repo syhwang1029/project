b3fadd0a405ac02a2addb0017127d2af0c98ef36
syhwang1029 <syhwang@itall.com>
2024-12-12 10:55:55 +0900

    user service

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,5 +1,4 @@
 from app.repository.user_repository import *
-from app.model.user import *
 
 # service 의존성 주입 참고 
 # https://the-boxer.tistory.com/63
@@ -9,14 +8,14 @@ class UserService:
     def __init__(self): # service가 repository에 의존
         self.user_repository = UserRepository()
         
-    def create_user(self): # 생성
-        return self.user_repository.create_user(User)
+    def create_user(self, user_data): # 생성
+        return self.user_repository.create_user(user_data)
     
     def read_user(self): # 조회
-        return self.user_repository.read_user(User)
+        return self.user_repository.read_user()
     
-    def update_user(self): # 수정
-        return self.user_repository.update_user(UserUpdate)
+    def update_user(self, user_data): # 수정
+        return self.user_repository.update_user(user_data)
     
-    def delete_user(self): # 삭제
-        return self.user_repository.delete_user(UserDelete)
+    def delete_user(self, user_id): # 삭제
+        return self.user_repository.delete_user(user_id)
