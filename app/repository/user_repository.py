d12a8a6c6467bec31333e23e1e08087adb7e4406
syhwang1029 <syhwang@itall.com>
2024-12-12 10:55:47 +0900

    user repository

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -13,52 +13,31 @@ collection = db["user"] # user collection 선택
 
 # user repository 
 class UserRepository: 
-    def __init__(self, create_user, read_user, update_user, delete_user): # user crud
-        self.create_user = create_user # 생성
-        self.read_user = read_user # 조회
-        self.update_user = update_user # 수정
-        self.delete_user = delete_user # 삭제   
 # 1. 생성 (create)   
-    def create_user(data): # user 생성
+    def create_user(self, data): # user 생성
         data = dict(data) # dict = {key:value}
         response = collection.insert_one(data) # response(응답) 
                         # db create 명령어 
         return str(response.inserted_id) #response : 등록 id
 # 2. 조회 (read)
-    def read_user(): # user 조회
+    def read_user(self): # user 조회
         response = collection.find()
                     # db read 명령어
         data = [] # List 초기화
-        for user in response:
-            user["_id"] = str(user["_id"]) # "_id"(ObjectId)로 user 검색 
-            data.append(user) # append : 데이터 추가
+        for users in response:
+            users["_id"] = str(users["_id"]) # "_id"(ObjectId)로 user 검색 
+            data.append(users) # append : 데이터 추가
             return data
 # 3. 수정 (update)
-    def update_user(data): # user 수정
+    def update_user(self, data): # user 수정
         data = dict(data)
-        response = collection.update_one({"_id": data["id"]},
+        response = collection.update_one({"_id": data["_id"]},
                                         {"$set": data})
                                 # db update 명령어
         return response.modified_count
 # 4. 삭제 (dlelte)
-    def delete_user(id): # user 삭제
+    def delete_user(self, id): # user 삭제
         response = collection.delete_one({"_id":id})
         # db delete 명령어
         return response.deleted_count
-    
-
-
-
-
-
-
-# def user_entity(user) -> dict:
-#     return{
-#         "id": str(user["_id"]),
-#         "name": user["name"],
-#         "email": user["email"],
-#         "password": user["password"] 
-#     }
-
-
 
