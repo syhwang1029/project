664aadffb31ab7294f59725056ba6fbcb1c9f20b
syhwang1029 <syhwang@itall.com>
2024-12-12 17:47:03 +0900

    main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # 전체 흐름 : main <- controller(router/domain) <- (model) <- service <- repository(dao) <- database(mongodb)
 from fastapi import FastAPI  #FastAPI 서버 #의존성 주입
 import uvicorn #main 함수로 uvicorn 자동 실행 
-from app.controller import user_controller #user 라우팅
+#from app.controller import user_controller  #user 라우팅
                     # import는 디렉토리명으로 해야함
-
+from app.controller import board_controller
 
 app = FastAPI() #app 인스턴스에 FastAPI 서버 할당
 
@@ -28,8 +28,8 @@ app = FastAPI() #app 인스턴스에 FastAPI 서버 할당
 #     return {"item_id": item_id}
 
 ## user ##
-app.include_router(user_controller.router)
-
+#app.include_router(user_controller.router)
+app.include_router(board_controller.router)
 ### FastAPI 서버 ####
 if __name__ == "__main__": # FastAPI 서버 자동 실행
     uvicorn.run(app, host="0.0.0.0", port=1203)
