51e24a4ca07e2008ad1ee5d4823b8afca4ed092c
syhwang1029 <syhwang@itall.com>
2024-12-12 10:55:30 +0900

    user model

--- app/model/user.py
+++ app/model/user.py
@@ -1,24 +1,22 @@
 # user schema
-from typing import Optional
-from bson import ObjectId
+from bson.objectid import ObjectId
 from pydantic import BaseModel, Field # Database 모델의 정의
+
 # 타입 검증, 오류 관리
 # https://mobicon.tistory.com/627
 
 # bson -> json 인코딩 역할 
 # user 데이터 모델 정의
 class User(BaseModel): # 조회, 생성
-    id: str = Field(alias="_id") 
-    name: str 
-    email: str
-    password: str
-    
-class UserUpdate(BaseModel): # 수정 
+    id: str = Field(alias="_id")
     name: str 
     email: str
     password: str
 
-class UserDelete(BaseModel): # 삭제
+class UserUpadat(BaseModel): # 수정
     name: str 
     email: str
     password: str
+
+class UserDelete(BaseModel): # 조회, 생성
+    id: str = Field(alias="_id")
