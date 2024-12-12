c940b26ce9776a5ec8d61723320cd66d18e322ed
syhwang1029 <syhwang@itall.com>
2024-12-12 10:56:13 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 print("test")
 
-# main -> controller(router/domain) -> service -> model -> repository(dao) -> database(mongodb)
-# mvc패턴 참고 
+# main <- controller(router/domain) <- (model) <- service <- repository(dao) <- database(mongodb)
+
+# FastAPI mvc패턴 참고
 # https://velog.io/@kku64r/fastapi
+
+# backend 시점의 mvc패턴 참고
+# https://diane21.tistory.com/24
