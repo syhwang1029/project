c06f991d2a6d81e233114bc975e6bf98d0e6bb57
syhwang1029 <syhwang@itall.com>
2024-12-13 11:54:29 +0900

    board model author명 변경

--- app/model/board.py
+++ app/model/board.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+
 # Board 데이터 모델 정의
 class Board(BaseModel):
     title: str # 게시판 제목
     content: str # 게시판 내용
-    author: str # 게시판 작성자
+    board_author: str # 게시판 작성자 
+# 댓글, 대댓글 author과 구분하기 위하여 앞에 "board"를 추가함 => board_author
+# 서로 다른 user 
