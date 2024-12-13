604a9005a37898c6e77dff26d134a4edc6096156
syhwang1029 <syhwang@itall.com>
2024-12-13 11:55:26 +0900

    board service

--- app/service/board_service.py
+++ app/service/board_service.py
@@ -1,11 +1,15 @@
 # board service 
-from app.repository.board_repository import BoardRepository
+from app.repository.board_repository import BoardRepository # board repository
+from app.model.board import Board # board model
 
+# 게시판
+# board repository 
 class BoardService(): 
-    def __init__(self): #board repository 인스턴스
-        self.repository = BoardRepository()
+    def __init__(self): # board repository 인스턴스
+        self.repository = BoardRepository() 
     
-    def create_service(self, board): # create
+    # 1. 생성 (create)
+    async def create_service(self, board: str): # create
         return self.repository.create_repository(board)
         
     
