887ac425010926d625a705dc629f3ccbc01c6a88
syhwang1029 <syhwang@itall.com>
2024-12-16 17:07:05 +0900

    board router 주석 제거

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # 전체 흐름 : main <- controller(router/domain) <- (model) <- service <- repository(dao) <- database(mongodb)
 from fastapi import FastAPI  #FastAPI 서버 #의존성 주입
 import uvicorn #main 함수로 uvicorn 자동 실행 
-from app.controller import user_controller  #user 라우팅
+from app.controller import user_controller  #user routing
                     # import는 디렉토리명으로 해야함
-from app.controller import board_controller
+from app.controller import board_controller # board routing
 
 app = FastAPI() #app 인스턴스에 FastAPI 서버 할당
 
 
-# ### route (end point) ###
-# # (특정) HTTP protocol : post(생성)/get(읽기)/put(수정)/delet(삭제)
+### route (endpoint) ###
+# (특정) HTTP protocol : post(생성)/get(읽기)/put(수정)/delet(삭제)
 
 ## user ##
 app.include_router(user_controller.router)
