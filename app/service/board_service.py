68aa6795f265237beb1e8efb818a4d01cb378d52
syhwang1029 <syhwang@itall.com>
2024-12-16 17:07:46 +0900

    비동기 def 추가

--- app/service/board_service.py
+++ app/service/board_service.py
@@ -8,19 +8,21 @@ class BoardService():
     def __init__(self): # board repository 인스턴스
         self.repository = BoardRepository() 
     
-        
     # 5. 전체 조회 (read) 
     def read_service_board(self):
         return self.repository.read_repository_board()
     
-    # 4. 조회 (read) 
+    # 4. 일부 조회 (read) 
     def read_service(self, board_id: str): #ObjectId
         return self.repository.read_repository(board_id)
         
+        
     # 1. 생성 (create) 
-    def create_service(self, board: Board): # create
+    async def create_service(self, board: Board): # create 
+        # 비동기
         board = dict(board) # data : dict 
-        return self.repository.create_repository(board)
+        return await self.repository.create_repository(board)
+            # 의존성 주입
 
     # 2. 수정 (update)
     def update_service(self, board_id: str, board: Board): # update
