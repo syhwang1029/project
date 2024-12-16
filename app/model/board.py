99e0e1dbff0de191a0ef825f28c3da29f791483d
syhwang1029 <syhwang@itall.com>
2024-12-16 17:07:55 +0900

    설명 추가

--- app/model/board.py
+++ app/model/board.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel
 
+# 게시판
 # Board 데이터 모델 정의
 class Board(BaseModel):
     title: str # 게시판 제목
     content: str # 게시판 내용
     board_author: str # 게시판 작성자 
-# 댓글, 대댓글 author과 구분하기 위하여 앞에 "board"를 추가함 => board_author
-# 서로 다른 user 
+# 댓글, 대댓글 author 구분  
+# "board" 추가 => board_author
+
+# 게시판, 댓글, 대댓글 => 서로 다른 user 
