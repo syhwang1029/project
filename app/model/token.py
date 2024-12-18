71e188a71717b920611780317f7c3bf2718428b1
syhwang1029 <syhwang@itall.com>
2024-12-18 17:38:05 +0900

    token

--- app/model/token.py
+++ app/model/token.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 
 # token 모델 데이터 정의
 class Token(BaseModel):
-    access_token: str
-    token_type: str
+    access_token: str # secret key
+    token_type: str # payload
     
 class TokenData(BaseModel):
     username: str | None = None
--- app/service/token_service.py
+++ app/service/token_service.py
@@ -1,1 +1,24 @@
 # token service
+from typing import Annotated
+
+from fastapi import Depends
+from app.repository.token_repository import TokenRepository
+from fastapi.security import OAuth2PasswordBearer
+from app.model.user import User
+        
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") 
+
+# token service
+class TokenService:
+    def __init__(self):
+        self.repository = TokenRepository()
+   
+    
+    def fake_decode_token(self, token):
+            return User( username = token + "fakedecoded",  
+                         email="john@example.com" )
+                   
+    async def get_crrent_uer(self, token: Annotated[str, Depends(oauth2_scheme)]):
+        user = fake_decode_token(token)
+        return user
+    
