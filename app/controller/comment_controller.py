20f91c9f4708f98751690eb1baab7f98f98970c5
syhwang1029 <syhwang@itall.com>
2024-12-27 15:50:50 +0900

    comment

--- app/controller/comment_controller.py
+++ app/controller/comment_controller.py
@@ -1,3 +1,5 @@
+from typing import List
+from bson import ObjectId
 from fastapi import APIRouter # router
 from app.service.comment_service import CommentService # comment service
 from app.model.comment import Comment, CommentUpdate # comment model
@@ -12,22 +14,23 @@ service =  CommentService() # comment service
 
 
 ## 댓글 (comment) ##
-# 4. 조회 (read)
-@router.get("/comment/",  response_model=list[Comment])# response_model : 응답 처리 model
-                            # list로 전체 comment 조회
-async def read_comment():
-    return await service.read_service()
-
-# 5. 일부 조회 (read)
-@router.get("/comment/{comment_id}", response_model=Comment)
-async def read_comment_comentid(comment_id: str):
-    return await service.read_service_commentid(comment_id)
-
-
 # 1. 생성 (create)
 @router.post("/comment/", response_model=Comment)
 async def create_comment(comment: Comment):
-    return await service.create_service(comment)
+    new_comment = await service.create_service(comment.model_dump()) # model_dump : json 형태로 변환
+    return await new_comment
+
+# 4. 전체 조회 (read)
+@router.get("/comment/", response_model=List[Comment]) # list 형태로 comment 전체 조회
+async def read_comment():
+    return await service.read_service()
+
+# 5. 일부 조회 (read) - comment id
+@router.get("/comment/{comment_id}", response_model=Comment)# response_model : 응답 처리 model
+                            # list로 전체 comment 조회
+async def raed_comment_commentid(comment_id: str):
+    comment = await service.read_service_commentid(ObjectId(comment_id))
+    return await comment
     
 # 2. 수정 (update)
 @router.put("/comment/{comment_id}", response_model=Comment)
@@ -37,4 +40,5 @@ async def update_comment(comment_id: str, comment: CommentUpdate):
 # 3. 삭제 (delete)
 @router.delete("/comment/{comment_id}", response_model=str)
 async def delete_comment(comment_id: str):
-    return await service.delete_service(comment_id)
+    comment_delete = await service.delete_service(ObjectId(comment_id))
+    return await comment_delete
--- app/repository/comment_repository.py
+++ app/repository/comment_repository.py
@@ -12,35 +12,38 @@ class CommentRepositoty:
         self.collection = collection # comment collection 
     
 # 1. 생성 (create)
-    async def create_repository(self, comment: str): # 새로운 comment 생성 
+    async def create_repository(self, comment): # 새로운 comment 생성 
         comments = collection.insert_one(comment) # comment 등록
-        return self.read_repository(comments.inserted_id) # objectid 자동 생성
-        
-# 2. 수정 (update)
-    async def update_repositoty(self, comment_id: str, comment): # comment id로 수정할 comment 지정
-        comments = collection.update_one({"_id":ObjectId(comment_id)},
-                                         {"$set": comment}) # comment 수정
-        return self.read_repository(comments.modified_count) # modified_count = 1 : 성공 / = 0 : 실패
+        return await self.read_repository(str(comments.inserted_id)) # objectid 자동 생성
 
-# 3. 삭제 (delete)
-    async def delete_repository(self, comment_id: str): # comment id로 comment 삭제
-        comments = collection.delete_one({"_id":ObjectId(comment_id)}) # comment 삭제
-        return comments.deleted_count > 0 # deleted_count = 1 : 성공
-
-# 4. 조회 (read)
-    async def read_repository(self): # 전체 comment 조회
-            commentlist = [] # comment의 data를 담을 list 초기화
-            comments = collection.find() # 전체 comment collection 조회
-            for comment in comments: # comment 조회
+# 5. 일부 조회 (read) - commentid
+    async def read_repository_commentid(self, comment_id: ObjectId): # 전체 comment 조회
+            comment = collection.find_one({"_id":comment_id}) # commentid = ObjectId
+            if comment: # comment collenction에서 data 조회
                 comment["_id"] = str(comment["_id"]) # objectid str 변환
                                                      # 이유 : pymongo로 불러온 mongodb 명령어는 
                                                      # 유효한 json 타입이 아님 
-                                                     # str 형변환 후 json 형태로 불러옴 
-                commentlist.append(comment) # list에 comment의 data 추가 
-            return commentlist # 추가된 commentlist로 comment 결과 반환
+                                                     # str 형변환 후 json 형태로 불러옴
+                del comment["_id"] # data가 없는 경우 objectid 삭제
+                return comment # 결과값 반환
 
-# 5. 일부 조회 (read) - commentid
-    async def read_repository_commentid(self, comment_id: str):  # objectid = comment_id                              
-        comments = collection.find_one({"_id":ObjectId(comment_id)}) # comment id로 comment 조회
-        comments["_id"] = str(comments) # comment id str 변환
-        return comments # comment 결과 반환
+# 4. 전체 조회 (raed) 
+    async def read_repository(self) -> list: # list로 comment 결과값 반환
+        commentlist = [] # comment의 data를 담을 list 초기화                          
+        comments = collection.find() # comment collection에서 data 전체 조회
+        for comment in comments: 
+            comment["id"] = str(comment["_id"]) # objectid str 변환 
+            del comment["_id"] # data가 없는 경우 objectid 삭제
+            commentlist.append(comment) # 조회한 data를 list에 추가
+        return commentlist # comment 결과값 반환
+            
+# 2. 수정 (update)
+    async def update_repositoty(self, comment_id: ObjectId, comment): # comment id로 수정할 comment 지정
+        collection.update_one({"_id":ObjectId(comment_id)},
+                                         {"$set": comment}) # comment 수정
+        return await self.read_repository(comment_id) # 수정 data 반환
+
+# 3. 삭제 (delete)
+    async def delete_repository(self, comment_id: ObjectId): # comment id로 comment 삭제
+        comments = collection.delete_one({"_id":comment_id}) # comment 삭제
+        return comments.deleted_count > 0 # deleted_count = 1 : 성공
