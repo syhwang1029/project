442dcc3375977d58fef1a05091e87ba41fa562ff
syhwang1029 <syhwang@itall.com>
2024-12-31 17:10:44 +0900

    self 추가로 comment 컬렉션과 reply 컬렉션 분명히 구분함

--- app/repository/comment_repository.py
+++ app/repository/comment_repository.py
@@ -20,14 +20,14 @@ class CommentRepositoty:
     
 # 1. 생성 (create) 
     async def create_repository(self, comment): # 새로운 comment 생성 
-        comments = collection.insert_one(comment) # mongodb 명령어로 새로운 comment의 data 등록
+        comments = self.collection.insert_one(comment) # mongodb 명령어로 새로운 comment의 data 등록
                     # 단일 문서로 comment 생성 
         return self.read_repository_commentid(comments.inserted_id) # objectid 자동 생성 
                             # objectid로 조회하는 함수(read_repository_commentid)로 반환
 
 # 5. 일부 조회 (read) - comment id
     async def read_repository_commentid(self, comment_id: ObjectId): # comment id로 일부 comment의 data 조회
-            comment = collection.find_one({"_id":comment_id}) # mongodb 명령어로 ObjectId로 지정한 comment id로 comment 조회 
+            comment = self.collection.find_one({"_id":comment_id}) # mongodb 명령어로 ObjectId로 지정한 comment id로 comment 조회 
             if comment:     
                 comment["_id"] = str(comment["_id"]) # objectid : str 변환
                                                      # 이유 : pymongo로 불러온 mongodb 명령어는 유효한 json 타입이 아님 
@@ -38,7 +38,7 @@ class CommentRepositoty:
 # 4. 전체 조회 (raed) 
     async def read_repository(self) -> list: # list로 comment 결과값 반환
         commentlist = [] # comment의 data를 담을 list 초기화                          
-        comments = collection.find({}) # mongodb명령어로 comment의 data 전체 조회
+        comments = self.collection.find({}) # mongodb명령어로 comment의 data 전체 조회
         for comment in comments: 
             comment["id"] = str(comment["_id"]) # objectid : str 변환 
             del comment["_id"] # data가 없는 경우 : objectid 삭제
@@ -54,7 +54,7 @@ class CommentRepositoty:
 
 # 3. 삭제 (delete)
     async def delete_repository(self, comment_id: ObjectId) -> bool: # comment id로 comment 삭제 
-        comments = collection.delete_one({"_id":comment_id}) # comment 삭제
+        comments = self.collection.delete_one({"_id":comment_id}) # comment 삭제
         return comments.deleted_count > 0 # deleted_count = 1 : 성공 # bool로 반환
 
     
